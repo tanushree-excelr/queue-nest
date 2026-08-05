5bf84d8d351c474208ee22514c8c31f13285dd71
tanushree-excelr <tanushreediwan98@gmail.com>
2026-08-05 13:04:07 +0530

    Fix BlockchainModule import in TransactionModule for 100% working APIs

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -3,9 +3,10 @@ import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
 import { QueueModule } from '../queue/queue.module';
 import { NonceModule } from '../nonce/nonce.module';
+import { BlockchainModule } from '../blockchain/blockchain.module';
 
 @Module({
-  imports: [QueueModule, NonceModule],
+  imports: [QueueModule, NonceModule, BlockchainModule],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
